58d3dd8aab1314396e77a7959eb590762c68b4b0
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-03-25 00:04:20 +0530

    Connecting to databse Creating entity for users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      //host: 'localhost',
+      //
+      port: 5432,
+      username: 'postgres',
+      password: 'divya123',
+      database: 'moodb',
+      entities: [],
+      synchronize: true,
+      logger:'advanced-console',
+      logging: 'all',  
+    }),
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
---
+++ src/users/users.entity.ts
@@ -0,0 +1,20 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity('Users')            //our users table will have these columns.
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('string',{length:30})
+  username:string;
+ 
+  @Column('string',{length:50})
+  name: string;
+
+  @Column('string')
+  avatar: string;
+
+  @Column('string',{length:240})
+  bio: string;
+}

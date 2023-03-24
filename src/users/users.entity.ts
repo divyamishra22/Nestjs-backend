2b8333cf5719da10f8c548105f038168f839927a
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-03-25 00:09:39 +0530

    Inheritance

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,8 +1,9 @@
+import { MooBaseEntity } from "src/commons/base.entity";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 
 @Entity('Users')            //our users table will have these columns.
-export class User {
+export class User extends MooBaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 

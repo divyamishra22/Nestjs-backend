34945614e3639380852c25cf219e14e4f5c45e90
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-03-25 18:18:08 +0530

    entities for session created

---
+++ src/auth/sessions.entity.ts
@@ -0,0 +1,14 @@
+import { MooBaseEntity } from "src/commons/base.entity";
+import { UserEntity } from "src/users/users.entity";
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+
+
+@Entity('sessions')
+export class SessionsEntity extends MooBaseEntity {
+  @Column()
+  userId: string;
+
+  @JoinColumn({ name: 'userId' })
+  @ManyToOne(() => UserEntity, { lazy: true })
+  user: Promise<UserEntity>;
+}

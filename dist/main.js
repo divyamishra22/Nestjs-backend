6b5173d007f35f4cbc8a423bae76dff5407f8155
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-03-30 03:49:22 +0530

    update

--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
-const platform_fastify_1 = require("@nestjs/platform-fastify");
 const swagger_1 = require("@nestjs/swagger");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_fastify_1.FastifyAdapter(), {
-        cors: {
-            origin: [
-                'http://ec2-13-51-249-139.eu-north-1.compute.amazonaws.com/'
-            ],
-        },
-    });
-    app.enableCors();
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const config = new swagger_1.DocumentBuilder()
         .setTitle('Moo API')
         .setDescription('API for checking user status')
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(), {
-      cors: {
-        origin: [
-          'http://ec2-13-51-249-139.eu-north-1.compute.amazonaws.com/'
+  // const app = await NestFactory.create<NestFastifyApplication>(
+  //   AppModule,
+  //   new FastifyAdapter(), {
+  //     cors: {
+  //       origin: [
+  //         'http://ec2-13-51-249-139.eu-north-1.compute.amazonaws.com/'
          
-        ],
-      },
-    }
-  );
+  //       ],
+  //     },
+  //   }
+  // );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Moo API')
